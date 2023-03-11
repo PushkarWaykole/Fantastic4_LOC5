8a8e74911bde5c884a36848110949d1dd4ecf919
Pushkar Waykole <pushkarwaykole123@gmail.com>
2023-03-11 13:43:27 +0530

    inital

---
+++ public/src/pages/Cards.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import Sample from './Sample.js';
+
+
+
+export default function Cards() {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  useEffect(() => {
+    const verifyUser = async () => {
+      if (!cookies.jwt) {
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "http://localhost:4000",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+        if (!data.status) {
+          removeCookie("jwt");
+          navigate("/login");
+        } else
+          toast(`Welcome ${data.user} 😎`, {
+            theme: "dark",
+          });
+      }
+    };
+    verifyUser();
+  }, [cookies, navigate, removeCookie]);
+
+  const logOut = () => {
+    removeCookie("jwt");
+    navigate("/login");
+  };
+  return (
+    <>
+      <div className="private">
+        <h1>Dashboard</h1>
+        {/* <Sample /> */}
+        <button onClick={logOut}>Log out</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
