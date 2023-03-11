f5620cf8c897b8ed4639d488e8825e87dcc6d4e8
Pushkar Waykole <pushkarwaykole123@gmail.com>
2023-03-11 19:41:26 +0530

    client2

---
+++ client_side/src/pages/auth/sign-in.jsx
@@ -0,0 +1,95 @@
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+
+export function SignIn() {
+  const [values, setValues] = useState({ email: "", password: "" });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5173/auth/sign-in",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      console.log("Data is: " + JSON.stringify(data));
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/dashboard/home");
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  return (
+    <>
+      <img
+        src="https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80"
+        className="absolute inset-0 z-0 h-full w-full object-cover"
+      />
+      <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
+      <div className="container mx-auto p-4">
+        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+          <CardHeader
+            variant="gradient"
+            color="blue"
+            className="mb-4 grid h-28 place-items-center"
+          >
+            <Typography variant="h3" color="white">
+              Sign In
+            </Typography>
+          </CardHeader>
+          <CardBody className="flex flex-col gap-4">
+            <Input name="email" type="email" label="Email" size="lg" onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }/>
+            <Input name="password" type="password" label="Password" size="lg" onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }/>
+            <div className="-ml-2.5">
+              <Checkbox label="Remember Me" />
+            </div>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button type="submit" variant="gradient" fullWidth onClick={(e)=>handleSubmit(e)}>
+              Sign In
+            </Button>
+            <Typography variant="small" className="mt-6 flex justify-center">
+              Don't have an account?
+              <Link to="/auth/sign-up">
+                <Typography
+                  as="span"
+                  variant="small"
+                  color="blue"
+                  className="ml-1 font-bold"
+                >
+                  Sign up
+                </Typography>
+              </Link>
+            </Typography>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default SignIn;
